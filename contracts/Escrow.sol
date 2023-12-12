6e0c508ff2dc7dbf05bd2bf470bae2c4af4d7e98
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-12 22:50:50 +0900

    escrow updated

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -8,6 +8,9 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Escrow {
   using SafeERC20 for IERC20;
 
+  bool public initialized = false;
+  address public tavern;
+  uint256 paymentAmount;
   address[] public tokens;
   address public governance; // who is it?
 
@@ -16,19 +19,36 @@ contract Escrow {
     _;
   }
 
+  modifier onlyTavern() {
+    require(msg.sender == tavern, "only tavern");
+    _;
+  }
+
   constructor (address[] memory _tokens) {
     tokens = _tokens;
     governance = msg.sender;
   }
 
-  function approve(address _token, address to, uint256 amount) public onlyGov {
-    IERC20(_token).approve(to, 0);
-    IERC20(_token).approve(to, amount);
+  function initialize() external payable returns (bool){
+    require(!initialized);
+    initialized = true;
+    tavern = msg.sender;
+    paymentAmount = msg.value;
+    return true;
   }
 
-  function transfer(address _token, address to, uint256 amount) public onlyGov {
-    IERC20(_token).transfer(to, amount);
+  function proccessPayment(address seeker) external{
+    (bool sent, bytes memory data) = seeker.call{value: paymentAmount}("");
+    require(sent, "Failed to send Ether");
   }
+  // function approve(address _token, address to, uint256 amount) public onlyGov {
+  //   IERC20(_token).approve(to, 0);
+  //   IERC20(_token).approve(to, amount);
+  // }
+
+  // function transfer(address _token, address to, uint256 amount) public onlyGov {
+  //   IERC20(_token).transfer(to, amount);
+  // }
 
   function setGovernance(address account) external onlyGov {
     governance = account;
