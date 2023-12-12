3a49c966b999c68b982b5ab55a8c12fea4fafb4a
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-12 22:51:01 +0900

    quest ipmlementation created

---
+++ contracts/Quest.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interfaces/ITavern.sol";
+
+/**
+ * @title Quest Implementation
+ * @dev Executes the proxy calls
+ */
+
+contract Quest {
+
+    bool public initialized = false;
+    address public tavern;
+    string infoURI;
+    //uint32 seekerId
+    address public solver;
+    address public seeker;
+
+    modifier onlySeeker() {
+    require(msg.sender == seeker, "only seeker");
+    _;
+    }
+
+    function initialize(
+        //uint32 _seekerId, 
+        address _solver,
+        address _seeker,
+        string memory _infoURI)
+        external
+        returns (bool)
+        {
+        require(!initialized);
+        initialized = true;
+        //seekerId = _seekerId;
+        solver = _solver;
+        seeker = _seeker;
+        tavern = msg.sender;
+        infoURI = _infoURI;
+        
+        return true;
+    }
+
+    function startDispute() external{}
+
+    function resolveDispute(uint8 seekerShare, uint8 solverShare) external{}
+
+    function finishQuest() external onlySeeker returns (bool) {
+        ITavern(tavern).callProccessPayment(seeker);
+        return true;
+    }
+}
