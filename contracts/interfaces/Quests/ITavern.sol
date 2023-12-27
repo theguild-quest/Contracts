47b491d10490637acd32a5ae705ba7475e051de0
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-27 10:41:07 +0900

    updated tavern interfac3

--- contracts/interfaces/Quests/ITavern.sol
+++ contracts/interfaces/Quests/ITavern.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.8.0;
 
 interface ITavern {
     function createNewQuest(address _solver, address _seeker, uint256 _paymentAmount, string memory infoURI) external payable;
-    function getEscrowImplementation() external view returns (address);
+    function escrowImplementation() external view returns (address);
+    function questImplementation() external view returns (address);
+    function barkeeper() external view returns (address);
+    function getProfileNFT() external view returns(address);
 }
